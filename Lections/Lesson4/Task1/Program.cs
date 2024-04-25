881a5777f2c6da29eeab756bf338f22ddd472575
Telli <tellihamzayeva@gmail.com>
2024-04-25 17:29:33 +0400

    Task2

--- Lections/Lesson4/Task1/Program.cs
+++ Lections/Lesson4/Task1/Program.cs
@@ -23,30 +23,30 @@
 int[,] CreateMatrix(int rowsCount, int columnsCount)
 {
     int[,] matrix = new int[rowsCount, columnsCount];
-Random rnd = new Random();
+    Random rnd = new Random();
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
 
-        matrix[i, j] = rnd.Next(1, 11);
+            matrix[i, j] = rnd.Next(1, 11);
+        }
     }
+    return matrix;
 }
-return matrix;
-}
-void ShowMatrix(int [,] matrix)
+void ShowMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
     {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
 
-        Console.Write($"{matrix[i, j]} ");
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-}
 }
 
-int[,] matrix= CreateMatrix(4,5);
+int[,] matrix = CreateMatrix(4, 5);
 ShowMatrix(matrix);
